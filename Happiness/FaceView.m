dcaaf2289315d547778c3a47c09b8b55168bcbad
robertlyc <merlinlyc@gmail.com>
2012-11-16 13:46:49 +0800

    draw circle

--- Happiness/FaceView.m
+++ Happiness/FaceView.m
@@ -6,6 +6,8 @@
 //  Copyright (c) 2012年 RoBeRt. All rights reserved.
 //
 
+#define DEFAULT_SCALE 0.90
+
 #import "FaceView.h"
 
 @implementation FaceView
@@ -19,14 +21,36 @@
     return self;
 }
 
+- (void)drawCircleAtPoint:(CGPoint)p withRadius:(CGFloat)radius inContext:(CGContextRef)context {
+    UIGraphicsPushContext(context);
+    CGContextBeginPath(context);
+    CGContextAddArc(context, p.x, p.y, radius, 0, 2*M_PI, YES);
+    CGContextStrokePath(context);
+    UIGraphicsPopContext();
+}
 
 // Only override drawRect: if you perform custom drawing.
 // An empty implementation adversely affects performance during animation.
 - (void)drawRect:(CGRect)rect {
     CGContextRef context = UIGraphicsGetCurrentContext();
     //draw face
+    CGPoint midPoint;
+    midPoint.x = self.bounds.origin.x + self.bounds.size.width / 2;
+    midPoint.y = self.bounds.origin.y + self.bounds.size.height / 2;
     
+    CGFloat size = self.bounds.size.width / 2;
+    if (self.bounds.size.height < self.bounds.size.width) {
+        size = self.bounds.size.height / 2;
+    }
+    size *= DEFAULT_SCALE;
+    
+    CGContextSetLineWidth(context, 5.0);
+    [[UIColor blueColor] setStroke];
+    
+    [self drawCircleAtPoint:midPoint withRadius:size inContext:context];
     //draw eyes
+    
+    
     
     //draw mouth
 }
